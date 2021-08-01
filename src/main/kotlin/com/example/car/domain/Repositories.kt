c5d5626e6e9b2927d87773510785425f8e79df06
Alex A <alexxsep@gmail.com>
2021-08-01 18:01:14 -0300

    Creating kotlin service

---
+++ src/main/kotlin/com/example/car/domain/Repositories.kt
@@ -0,0 +1,10 @@
+package com.example.car.domain
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+
+interface DriverRepository : JpaRepository<Driver, Long>
+
+interface PassengerRepository : JpaRepository<Passenger, Long>
+
+interface TravelRequestRepository : JpaRepository<TravelRequest, Long>
